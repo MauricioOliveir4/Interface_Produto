f0d6efc358b4cc4c1bc0195cd8f86f391624f24d
Mauricio Oliveira silva <142525618+MauricioOliveir4@users.noreply.github.com>
2024-05-13 20:32:08 -0300

    Comida.java

---
+++ Comida.java
@@ -0,0 +1,11 @@
+public class Comida implements IProduto{
+    private String nome;
+    private double preco;
+    private String descricao;
+}
+  public double getpreco() {
+      this.preco return preco;
+  }
+      
+      
+  
