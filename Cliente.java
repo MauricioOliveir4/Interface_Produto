16e7224cf5588f74830eda083feccd4c43e46f90
Mauricio Oliveira silva <142525618+MauricioOliveir4@users.noreply.github.com>
2024-05-13 20:34:12 -0300

    Cliente.java

---
+++ Cliente.java
@@ -0,0 +1,10 @@
+public class Cliente {
+    private String nome;
+    private String email;
+    }
+    public double getNome() {
+      this.nome return nome;
+    }
+    public double getEmail() {
+      this.email return email;
+    }
