efa1459d5a2f3375d7898e99e935f79badea61d9
Mauricio Oliveira silva <142525618+MauricioOliveir4@users.noreply.github.com>
2024-05-13 20:32:44 -0300

    Bebida.java

---
+++ Bebida.java
@@ -0,0 +1,14 @@
+public class bebida{
+    private String nome;
+    private String descricao;
+    private double preco;
+}
+
+public double getPreco() {
+      this.preco return preco;
+}
+public double getDescricao() {
+      this.descricao return descricao;
+}
+    
+    
